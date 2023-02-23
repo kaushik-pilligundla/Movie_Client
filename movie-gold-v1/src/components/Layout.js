3e909156a138430af508cd35b8e54bf106396d4e
kaushik-pilligundla <kaushikpilligundla@gmail.com>
2023-02-23 12:12:16 -0800

    final

---
+++ movie-gold-v1/src/components/Layout.js
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+import Reaxct from 'react'
+
+const Layout = () => {
+    return (
+        <main>
+            <Outlet/>
+        </main>
+    )
+}
+
+export default Layout
---
+++ movie-gold-v1/src/components/home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Hero from '../hero/Hero'
+
+const home = ({movies}) => {
+  return (
+    <Hero movies = {movies}/>
+  )
+}
+
+export default home
+ 
